97eb976071220bf57e3bf484ffef3e3c210e4842
arthurfary <arthurfary22@gmail.com>
2023-11-21 14:38:35 -0300

    Primeira metade da home, falta:
    - adicionar o resto das imagens
    - criar o resto da home
    
    se alguem tiver coragem de refatorar o css para nao usar tantos transform-translate = LENDA

--- app/components/hero_maker/ImageHero.jsx
+++ app/components/hero_maker/ImageHero.jsx
@@ -2,10 +2,10 @@
 import styles from './back.module.css';
 import Image from 'next/image';
 
-const BackImage = ({ children, src, height, opacity = 0 }) => {
+const BackImage = ({ children, src, height, opacity = 0, pullUp = 0, cover = false }) => {
     return (
         <div style={{ position: 'relative', width: '100%', height: `${height}vh` }}>
-            <Image src={src} layout='fill' objectFit='cover' objectPosition='center' />
+            <Image src={src} layout='fill' objectFit={cover ? "cover" : ""} objectPosition='center' style={{ transform: `translateY(-${pullUp}em)`, zIndex: -1}}/>
             <div style={{ position: 'absolute', top: 0, left: 0, bottom: 0, right: 0, backgroundColor: `rgba(255, 255, 255, ${opacity})` }}></div>
             <div style={{ position: 'absolute', zIndex: 1 }}>
                 {children}
@@ -14,12 +14,12 @@ const BackImage = ({ children, src, height, opacity = 0 }) => {
     )
 }
 
-const HeroImage = ({ children, src, height, opacity }) => {
+const HeroImage = ({ children, src, height, opacity, pullUp, cover }) => {
     return (
-        <BackImage src={src} height={height} opacity={opacity} alt='Hero image'>
+        <BackImage src={src} height={height} opacity={opacity} pullUp={pullUp} cover={cover} alt='Hero image'>
           {children}
         </BackImage>
     );
-  };
+};
 
 export default HeroImage;
--- app/page.jsx
+++ app/page.jsx
@@ -1,19 +1,72 @@
 import styles from './page.module.css'
 
-import FotoFundo from '../public/foto_fundo_home.jpeg'
+import BackgroundImage from '../public/foto_fundo_home.png'
+import CatImage from '../public/gato_home.png'
+import DogImage from '../public/cachorro_home.png'
+import CircleDogImage from '../public/cachorro_ciruclo.png'
+import LinesImage from '../public/lines_home.png'
+import BallAndBoneImage from '@/public/ball_and_bone_home.png'
+import PawsImage from '@/public/patas_home.png'
 
 import SitePage from './components/page_type/site'
-import HeroImage from './components/hero_maker/ImageHero'
+import ImageHero from './components/hero_maker/ImageHero'
+import ColorHero from './components/hero_maker/ColorHero'
+
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <SitePage>
       <main className={styles.main}>
-        <HeroImage src={FotoFundo} height={100} opacity={.1}>
-          <div className={styles.herodiv}>
-            <h1>Bem vindo a APA!</h1>
+        <ImageHero src={BackgroundImage} height={115} pullUp={5}>
+          <div className={styles.heroContainer}>
+            <div className={styles.imageContainer}>
+              <div className={styles.singleImageContainer}>
+                <Image className={styles.responsiveImage} src={CatImage}></Image>
+              </div>
+              <div className={styles.singleImageContainer}>
+                <Image className={styles.responsiveImage} src={DogImage}></Image>
+              </div>
+            </div>
+
+            <div className={styles.textBox}>
+              <h1>APA</h1>
+              <p>Associação Protetora dos Animais</p>
+            </div>
           </div>
-        </HeroImage>
+        </ImageHero>
+
+        {/* Imagens entre o hero e o container */}
+        <div className={styles.linesContainer}>
+          <Image src={LinesImage} className={styles.linesImage}></Image>
+        </div>
+
+        <div className={styles.ballAndBoneContainer}>
+          <Image src={BallAndBoneImage} className={styles.ballAndBoneImage}></Image>
+        </div>
+
+        <div className={styles.pawsContainer}>
+          <Image src={PawsImage} className={styles.pawsImage}></Image>
+        </div>
+
+        <div className={styles.helpContainer}>
+          
+
+          <div className={styles.helpText}> 
+            <h1>Como ajudar</h1>
+            <p>Você pode nos ajudar com doações, sendo um voluntário ou adotando um pet!</p>
+          </div>
+
+          <div className={styles.dogCircle}>
+            <Image className={styles.circleDogImage} src={CircleDogImage}></Image>
+          </div>
+
+        </div>
+
+        <ColorHero color={'var(--primary-color)'} height={100}>
+          <h1>teste</h1>
+        </ColorHero>
+
         <h2>test</h2>
       </main>
     </SitePage>
