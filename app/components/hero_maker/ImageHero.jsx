3efb69255e44341ad27dc40c5ff67ab4a428da95
arthurfary <arthurfary22@gmail.com>
2023-11-27 13:57:33 -0300

    Css Mais Responsivo

--- app/components/hero_maker/ImageHero.jsx
+++ app/components/hero_maker/ImageHero.jsx
@@ -5,7 +5,7 @@ const HeroImage = ({ children, src, height, pullUp = 0, cover = false }) => {
     return (
         <div style={{ position: 'relative', width: '100%', height: `${height}vh` }}>
             <Image src={src} layout='fill' objectFit={cover ? true : ""} objectPosition='center' style={{ transform: `translateY(-${pullUp}em)`, zIndex: -1}}/>
-            <div style={{ position: 'absolute', top: 0, left: 0, bottom: 0, right: 0, display: 'flex', justifyContent: 'center', alignItems: 'center', zIndex: 1 }}>
+            <div style={{ left: 0, bottom: 0, right: 0, display: 'flex', justifyContent: 'center', alignItems: 'center', zIndex: 1}}>
                 {children}
             </div>
         </div>
