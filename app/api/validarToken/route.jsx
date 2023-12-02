5f714f3ab329502a77f4d2d50875fddbafa853f9
Freddy Albert Baier <95866940+Fooyer@users.noreply.github.com>
2023-12-02 00:14:05 +0000

    teste conf supa

--- app/api/validarToken/route.jsx
+++ app/api/validarToken/route.jsx
@@ -1,40 +1,37 @@
+import { NextResponse } from "next/server";
+
 import client from "../database";
 
-// This function checks if a given string is a valid UUID.
+export async function GET(request) {
+
+    if (request.credentials !== "same-origin") {
+        return NextResponse.json({message: "Não Autorizado!"},{ status: 403 });
+    }
+
+    // pega parametros
+    const parameters = request.nextUrl.searchParams;
+
+    let token = parameters.get('token')
+    let nome = parameters.get('nome')
+
+    if (!isUUID(token)) {
+        return NextResponse.json({message: "Não Autorizado!"},{ status: 401 });
+    }
+
+    if (token == "undefined" || nome == "undefined"){
+        return NextResponse.json({message: "Não Autorizado!"},{ status: 401 });
+    }
+
+    const { rows } = await client.query("SELECT id FROM usuarios WHERE token = $1 and nome = $2", [token,nome]);
+
+    if (rows == ''){
+        return NextResponse.json({message: "Não Autorizado!"},{ status: 401 });
+    }
+
+    return NextResponse.json({message: "Logado com sucesso!"}, { status: 200 });
+}
+
 function isUUID(str) {
     const uuidPattern = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/;
     return uuidPattern.test(str);
-}
-
-// API route handler
-export default async function handler(req, res) {
-    // Only allow GET requests
-    if (req.method !== 'GET') {
-        return res.status(405).send({ message: "Method Not Allowed" });
-    }
-
-    // Extract parameters from the query string
-    const { token, nome } = req.query;
-
-    // Check if the token and nome are valid
-    if (!token || !nome || !isUUID(token)) {
-        return res.status(401).send({ message: "Não Autorizado!" });
-    }
-
-    try {
-        // Query the database
-        const { rows } = await client.query("SELECT id FROM usuarios WHERE token = $1 and nome = $2", [token, nome]);
-        
-        // Check if any user is found
-        if (rows.length === 0) {
-            return res.status(401).send({ message: "Não Autorizado!" });
-        }
-
-        // Successful authentication
-        return res.status(200).send({ message: "Logado com sucesso!" });
-    } catch (error) {
-        // Handle any unexpected errors
-        console.error(error);
-        return res.status(500).send({ message: "Internal Server Error" });
-    }
 }
