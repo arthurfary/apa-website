d0613eadcd78a5505d0a696bdaa5b4c88b479e86
Freddy Albert Baier <95866940+Fooyer@users.noreply.github.com>
2023-12-02 00:35:58 +0000

    teste com validarLogin

--- app/api/validarLogin/route.jsx
+++ app/api/validarLogin/route.jsx
@@ -1,26 +1,47 @@
-import { NextResponse } from "next/server";
-
-import client from "../database";
+import { NextResponse } from 'next/server';
+import client from '../database';
 
 export async function POST(request) {
-
-    if (request.credentials !== "same-origin") {
-        return NextResponse.json({message: "Não Autorizado!"},{ status: 403 });
+    // Ensure the request is of same origin for security
+    if (request.credentials !== 'same-origin') {
+        return NextResponse.json({ message: 'Não Autorizado!' }, { status: 403 });
     }
 
-    const body = await request.json()
+    // Parse the request body
+    const body = await request.json();
+    const { nome, senha } = body;
 
-    let nome = body.nome
-    let senha = body.senha
+    try {
+        // Check credentials
+        const { rows } = await client.query(
+            'SELECT id, nome FROM usuarios WHERE nome = $1 AND senha = $2',
+            [nome, senha]
+        );
 
-    const { rows } = await client.query("SELECT id,nome FROM usuarios WHERE nome = $1 AND senha = $2", [nome, senha]);
+        if (rows.length === 0) {
+            return NextResponse.json({ message: 'Credenciais inválidas!', autorizado: 0 }, { status: 401 });
+        }
 
-    if (rows == '') {
-        return NextResponse.json({message: "Credenciais inválidas!", autorizado: 0},{ status: 401 });
+        // Update token
+        await client.query(
+            'UPDATE usuarios SET token = uuid_generate_v4() WHERE nome = $1 AND senha = $2',
+            [nome, senha]
+        );
+
+        // Retrieve updated token
+        const { rows: tokenRows } = await client.query(
+            'SELECT token FROM usuarios WHERE nome = $1 AND senha = $2',
+            [nome, senha]
+        );
+
+        return NextResponse.json({
+            message: 'Logado com sucesso!',
+            user_data: rows[0],
+            autorizado: 1,
+            token: tokenRows[0].token
+        }, { status: 200 });
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
     }
-
-    const { res } = await client.query("UPDATE usuarios SET token = uuid_generate_v4() WHERE nome = $1 AND senha = $2", [nome, senha]);
-    const { rows: token } = await client.query("SELECT token FROM usuarios WHERE nome = $1 AND senha = $2", [nome, senha]);
-
-    return NextResponse.json({message: "Logado com sucesso!",user_data: rows[0], autorizado: 1, token: token[0].token}, { status: 200 });
 }
