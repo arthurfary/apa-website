595c363861e30f3ec04affce6548872d1a57062b
Freddy Albert Baier <95866940+Fooyer@users.noreply.github.com>
2023-12-02 00:47:23 +0000

    config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  output: 'export',
   images: { unoptimized: true },
  
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
