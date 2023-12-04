612cf13d168a2293bc88cf786ece4b36c43a27d5
Freddy Albert Baier <95866940+Fooyer@users.noreply.github.com>
2023-12-04 16:41:01 +0000

    error 404

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -2,16 +2,17 @@
 
 import React from 'react';
 import Link from 'next/link';
-import styles from './404.module.css'; // Importando o módulo CSS
+import styles from './not-found.module.css';
+import SitePage from './components/page_type/site';
 
 export default function Custom404() {
     return (
-        <div className={styles.errorContainer}>
-            <h1 className={styles.errorTitle}>404</h1>
-            <p className={styles.errorMessage}>Página não encontrada</p>
-            <Link href="/">
-                Voltar para a página inicial
-            </Link>
-        </div>
+        <SitePage>
+            <div className={styles.errorContainer}>
+                <h1 className={styles.errorTitle}>Error 404</h1>
+                <p className={styles.errorMessage}>Página não encontrada</p>
+                <Link className={styles.homeLink} href="/">Voltar para a página inicial</Link>
+            </div>
+        </SitePage>
     );
 }
