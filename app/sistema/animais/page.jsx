bcd350236fc44089ff17c144a79f81a99937b4f4
Freddy Albert Baier <95866940+Fooyer@users.noreply.github.com>
2023-12-09 02:22:53 +0000

    mod 1

--- app/sistema/animais/page.jsx
+++ app/sistema/animais/page.jsx
@@ -1,66 +1,96 @@
 'use client'
 
-import styles from '@/app/sistema/animais/page.module.css';
-
-import { useEffect, useState } from 'react';
-
+import React, { useEffect, useState } from 'react';
 import SystemPage from '@/app/components/page_type/sistema';
 import Load from '@/app/components/load/load';
 import Card from './card/card';
 import ImageUpload from '@/app/components/image_upload/image_upload';
 
-function Animais(){
+import styles from './page.module.css'; // Substitua pelo caminho correto do seu arquivo de estilos
 
+function Animais() {
     const [animais, setAnimais] = useState([]);
     const [showAddForm, setShowAddForm] = useState(false);
-    const [image, setImage] = useState(false);
+    const [image, setImage] = useState(null);
     const [refresh, setRefresh] = useState(false);
     const [loading, setLoading] = useState(false);
+    const [nome, setNome] = useState('');
+    const [raca, setRaca] = useState('');
+    const [descricao, setDescricao] = useState('');
+    const [idade, setIdade] = useState('');
+    const [unidadeTempo, setUnidadeTempo] = useState('semanas');
+    const [porte, setPorte] = useState('pequeno'); // Novo estado para o porte
 
     useEffect(() => {
-        fetch('/api/obterPets',{
+        fetch('/api/obterPets', {
             headers: {
-                'Cache-Control': 'no-cache, no-store, must-revalidate', // Impede o cache
-                'Pragma': 'no-cache', // Suporte para navegadores mais antigos
-                'Expires': '0' // Data de expiração passada para impedir o cache
+                'Cache-Control': 'no-cache, no-store, must-revalidate',
+                'Pragma': 'no-cache',
+                'Expires': '0'
             }
         })
-            .then(response => response.json())
-            .then(data => setAnimais(data.rows));
+        .then(response => response.json())
+        .then(data => setAnimais(data.rows));
     }, [refresh]);
 
+    // Função para alternar entre a exibição do formulário e o botão de adição
     function alteraCard() {
         setShowAddForm(!showAddForm);
-        setImage(false)
-    };
+        setImage(null);
+        setNome('');
+        setRaca('');
+        setDescricao('');
+        setIdade('');
+        setUnidadeTempo('semanas');
+        setPorte('pequeno'); // Defina o porte de volta para o valor padrão 'pequeno'
+    }
 
-    async function salvarNovo(){
-        setLoading(true)
-        const nome = document.querySelector('input[name="nome"]').value;
-        const especie = document.querySelector('input[name="especie"]').value;
-        const foto = image;
+    async function salvarNovo(e) {
+        e.preventDefault()
+        setLoading(true);
 
-        if (!nome || !especie || !foto) {
-            alert('Preencha todos os campos!');
-            setLoading(false)
+        if (!nome || !raca || !descricao || !image || !porte) {
+            alert('Preencha todos os campos obrigatórios!');
+            setLoading(false);
             return;
         }
+
+        const idadeDias = idade ? calcularIdadeEmDias(idade, unidadeTempo) : null;
 
         await fetch('/api/salvarPet', {
             method: 'POST',
             body: JSON.stringify({
-                nome: nome,
-                especie: especie,
-                foto: foto
+                nome,
+                raca,
+                descricao,
+                idade: idadeDias,
+                foto: image,
+                porte
             })
         })
-            .then(response => response.json())
-            .then(data => {
-                setRefresh(!refresh);
-                alteraCard();
-                setLoading(false)
-            });
-        setLoading(false)
+        .then(response => response.json())
+        .then(data => {
+            setRefresh(!refresh);
+            alteraCard();
+            setLoading(false);
+        })
+        .catch(error => {
+            console.error('Erro ao salvar:', error);
+            setLoading(false);
+        });
+    }
+
+    function calcularIdadeEmDias(idade, unidadeTempo) {
+        const idadeNumero = parseFloat(idade);
+        let multiplicador = 1;
+
+        if (unidadeTempo === 'meses') {
+            multiplicador = 30;
+        } else if (unidadeTempo === 'anos') {
+            multiplicador = 365;
+        }
+
+        return idadeNumero * multiplicador;
     }
 
     return (
@@ -72,32 +102,82 @@ function Animais(){
 
                 <section className={styles.cardGrid}>
                     {showAddForm ? (
-                        <div className={styles.addCardForm}>
-                            <input className={styles.inputField} type="text" placeholder="Nome" name='nome' />
-                            <input className={styles.inputField} type="text" placeholder="Espécie" name='especie' />
+                        <div className={styles.addCard}>
+                        <form className={styles.addCardForm}>
                             <ImageUpload setImage={setImage} image={image} />
-                            {loading === false && 
+                            <input
+                                className={styles.inputField}
+                                type="text"
+                                placeholder="Nome"
+                                value={nome}
+                                onChange={(e) => setNome(e.target.value)}
+                            />
+                            <input
+                                className={styles.inputField}
+                                type="text"
+                                placeholder="Raça"
+                                value={raca}
+                                onChange={(e) => setRaca(e.target.value)}
+                            />
+                            <textarea
+                                className={styles.textArea}
+                                placeholder="Descrição (máximo de 100 caracteres)"
+                                maxLength={100}
+                                value={descricao}
+                                onChange={(e) => setDescricao(e.target.value)}
+                            />
+                            <div className={styles.inputContainer}>
+                                <input
+                                    className={styles.inputField}
+                                    type="number"
+                                    placeholder="Idade"
+                                    value={idade}
+                                    onChange={(e) => setIdade(e.target.value)}
+                                />
+                                <select
+                                    className={styles.selectUnidadeTempo}
+                                    value={unidadeTempo}
+                                    onChange={(e) => setUnidadeTempo(e.target.value)}
+                                >
+                                    <option value="semanas">Semanas</option>
+                                    <option value="meses">Meses</option>
+                                    <option value="anos">Anos</option>
+                                </select>
+                            </div>
+                            <select
+                                className={styles.selectPorte}
+                                value={porte}
+                                onChange={(e) => setPorte(e.target.value)}
+                            >
+                                <option value="pequeno">Pequeno</option>
+                                <option value="medio">Médio</option>
+                                <option value="grande">Grande</option>
+                            </select>
+                            {loading === false && (
                                 <div>
-                                    <button className={`${styles.button} ${styles.salvar}`} onClick={salvarNovo} >Salvar</button>
-                                    <button className={`${styles.button} ${styles.cancelar}`} onClick={alteraCard} >Cancelar</button>
+                                    <button className={`${styles.button} ${styles.salvar}`} onClick={salvarNovo}>
+                                        Salvar
+                                    </button>
+                                    <button className={`${styles.button} ${styles.cancelar}`} onClick={alteraCard}>
+                                        Cancelar
+                                    </button>
                                 </div>
-                            }
-                            {loading === true &&
+                            )}
+                            {loading === true && (
                                 <div className={styles.loading}>
-                                    <Load size={30}/>
+                                    <Load size={30} />
                                 </div>
-                            }
-                        </div>
+                            )}
+                        </form>
+                    </div>
                     ) : (
-                        <div className={`${styles.cardCommon} ${styles.addCard}`} onClick={alteraCard}>
-                            +
+                        <div className={styles.cardAdd} onClick={alteraCard}>
+                            <span>+</span>
                         </div>
                     )}
 
                     {animais.map((animal, index) => (
-
-                        <Card id={animal.id} nome={animal.nome} especie={animal.especie} foto={animal.foto} setRefresh={setRefresh} refresh={refresh} />
-
+                        <Card key={index} id={animal.id} nome={animal.nome} raca={animal.raca} descricao={animal.descricao} idade={animal.idade} foto={animal.foto} setRefresh={setRefresh} refresh={refresh} />
                     ))}
                 </section>
             </main>
