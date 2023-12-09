e00ca6f1a239477de6df79d100bb919d61ecbb60
Fooyer <xneerd157@gmail.com>
2023-12-09 20:40:58 -0300

    teste

--- app/api/obterNoticias/route.jsx
+++ app/api/obterNoticias/route.jsx
@@ -2,11 +2,15 @@ import { NextResponse } from "next/server";
 import client from "../database";
 
 export async function GET(request) {
-    NextResponse.setHeader('Cache-Control', 'no-store, max-age=0');
+    // Cria uma resposta usando NextResponse
+    const response = NextResponse.next();
+
+    // Define os cabeçalhos para desabilitar o cache
+    response.headers.set('Cache-Control', 'no-store, max-age=0');
 
     // Verifica se as credenciais são do mesmo domínio
     if (request.credentials !== "same-origin") {
-        return NextResponse.json({ message: "Não Autorizado!", success: 0 }, { status: 403 });
+        return response.json({ message: "Não Autorizado!", success: 0 }, { status: 403 });
     }
 
     try {
@@ -15,14 +19,14 @@ export async function GET(request) {
 
         // Verifica se existem notícias cadastradas
         if (rows.length === 0) {
-            return NextResponse.json({ message: "Não existem notícias cadastradas!", rows, success: 1 }, { status: 200 });
+            return response.json({ message: "Não existem notícias cadastradas!", rows, success: 1 }, { status: 200 });
         }
 
         // Retorna as notícias encontradas
-        return NextResponse.json({ rows, success: 1 }, { status: 200 });
+        return response.json({ rows, success: 1 }, { status: 200 });
     } catch (error) {
         // Trata possíveis erros durante a execução da query
         console.error(error);
-        return NextResponse.json({ message: "Erro ao obter as notícias.", success: 0 }, { status: 500 });
+        return response.json({ message: "Erro ao obter as notícias.", success: 0 }, { status: 500 });
     }
 }
